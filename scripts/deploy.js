1e7d21f207a63044308884b57d542fff5c5544ee
Kingsley Victor <javaprodigy56@gmail.com>
2022-10-28 07:50:33 +0100

    ...

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,25 @@
 const { ethers, network } = require("hardhat");
 const path = require("path");
 const fs = require("fs");
+const axios = require("axios");
+
+const coinGeckID = {
+  56: "binancecoin",
+  32520: "bitrise-token",
+  311: "omax-token",
+  86: "gatechain-token"
+};
 
 (async () => {
   console.log("---------- Deploying to chain %d ----------", network.config.chainId);
   const MultiSigActionsFactory = await ethers.getContractFactory("MultiSigActions");
 
-  let multiSigActions = await MultiSigActionsFactory.deploy(ethers.utils.parseEther("0.00003"));
+  const cgID = coinGeckID[network.config.chainId];
+  const { data } = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${cgID}&vs_currencies=usd`);
+  const valInUSD = data[cgID].usd;
+  const valEther = 10 / valInUSD;
+
+  let multiSigActions = await MultiSigActionsFactory.deploy(ethers.utils.parseEther(valEther.toString()));
   multiSigActions = await multiSigActions.deployed();
 
   const location = path.join(__dirname, "../actions_addresses.json");
